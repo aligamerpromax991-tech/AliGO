0e96917de9ac02f60c9c518c5e08474ee1157907
aligamerpromax991-tech <aligamerpromax991@gmail.com>
2026-08-28 20:52:31 +0400

    Update AliGO.py

--- AliGO.py
+++ AliGO.py
@@ -612,7 +612,7 @@ def ask_groq(messages_history, user_plan="UltiPremium", mode="chat"):
         })
 
     payload = {
-        "model": "llama-3.1-8b-instant",
+        "model": "llama-3.3-70b-versatile",
         "messages": formatted_messages,
         "temperature": st.session_state.ai_temp,
         "max_tokens": 1500
